24ddbfdf127c4e50a478e6f54fe42641b35cafce
coDribble <sksrlatkddn@naver.com>
2021-01-24 01:47:54 +0900

    210124 Username Persistance.

--- static/js/whatyourname.js
+++ static/js/whatyourname.js
@@ -1,4 +1,9 @@
+const form = document.getElementById('askName');
+const toDo = document.getElementById('toDo');
+const intro = toDo.querySelector('.intro');
 const somethingName = document.getElementById('name');
+
+const NAME_LS = 'name';
 
 function handleName(e){
     const name = e.target.value;
@@ -9,7 +14,29 @@ function handleName(e){
         somethingName.classList.remove('on');
 }
 
+function whoareyou(){
+    const yourName = localStorage.getItem(NAME_LS);
+
+    if( yourName === null ){
+        form.style.display = 'block';
+        toDo.style.display = 'none';
+    }else{
+        form.style.display = 'none';
+        toDo.style.display = 'block';
+        intro.innerText = `Welcome! ${yourName}!`;
+    }
+}
+
+function handleMemoryName(){
+    const who = somethingName.value;
+
+    localStorage.setItem(NAME_LS, who);
+}
+
 function init(){
+    whoareyou();
+
+    form.addEventListener('submit', handleMemoryName);
     somethingName.addEventListener('keyup', handleName);
 }
 
